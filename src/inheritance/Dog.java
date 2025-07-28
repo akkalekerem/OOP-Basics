334beb9a9966b1a3bd2d51803961d413a3c74ce6
Kerem Akkale <keremakkale@gmail.com>
2025-07-28 14:21:47 +0300

    Inheritance Örneği

---
+++ src/inheritance/Dog.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class Dog extends Animal { //Bu kısım önemli (extends)!!!
+    @Override
+    public void speak(){
+        System.out.println("Dog barks.");
+    }
+
+}
